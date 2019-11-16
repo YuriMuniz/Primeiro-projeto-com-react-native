2a1d664eacefd83b6661df7f95446ba83b2690b4
YMG <y.munizgonzalez@gmail.com>
2019-11-16 19:26:39 -0300

    configura react-navigation

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
 
 import './config/ReactotronConfig';
 
-const styles = StyleSheet.create({
-    welcome: {
-        fontSize: 20,
-    },
-});
+import Routes from './routes';
 
-console.tron.warn('Hello World');
 export default function App() {
-    return (
-        <View>
-            <Text style={styles.welcome}>Texto</Text>
-        </View>
-    );
+    return <Routes />;
 }
